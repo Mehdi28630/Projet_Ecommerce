58ecc481fd5122e4d77d09d40172c558389d73b1
Mehdi28630 <78013620+Mehdi28630@users.noreply.github.com>
2022-05-09 17:45:53 +0200

    presque ajout du panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,18 +3,51 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Repository\ProduitRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", name="cart")
+     * @Route("/cart", name="cart_index")
      */
-    public function index(): Response
+    public function index(Request $request, ProduitRepository $produitRepository)
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+        $session = $request->getSession();
+        $panier = $session->get('panier', []);
+
+        $panierWithData = [];
+
+        foreach($panier as $id => $quantite){
+            $panierWithData[] = [
+                'produit' => $produitRepository->find($id),
+                'quantité' => $quantite,
+            ];
+        }
+        dd($panierWithData);
+
+        return $this->render('cart/index.html.twig',[
+            'items' => $panierWithData
         ]);
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="cart_add")
+     */
+    public function add($id, Request $request)
+    {
+        $session = $request->getSession();
+        $panier = $session->get('panier', []);
+
+        if(!empty($panier[$id])) {
+            $panier[$id]++;
+        }
+        else{
+            $panier[$id] = 1;
+        }
+
+        $session->set('panier',$panier);
+        dd($session->get('panier'));
     }
 }
